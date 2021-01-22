cb5d150cc4699aabc0e747624369cc1fece7d186
Tiago Peres França <tiagoperesfr@gmail.com>
2021-01-22 12:08:49 -0300

    step 4

--- angular/src/app/beagle-components.module.ts
+++ angular/src/app/beagle-components.module.ts
@@ -3,7 +3,6 @@ import { NgModule, Type } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { InputContainerComponent } from './components/input-container/input-container.component';
 import { MoneyInputComponent } from './components/money-input/money-input.component';
 import { RadioGroupComponent } from './components/radio-group/radio-group.component';
 import { SelectComponent } from './components/select/select.component';
@@ -14,8 +13,7 @@ const components: (any[] | Type<any>)[] = [
     TitleComponent,
     RadioGroupComponent,
     SelectComponent,
-    MoneyInputComponent,
-    InputContainerComponent
+    MoneyInputComponent
 ];
 
 @NgModule({
